bc517023c8d578fe32022b2d46737057e8d50766
Pawel Wrobel <wroobel.p@gmail.com>
2023-10-13 09:54:12 +0200

    n

---
+++ lab1/hello_world.py
@@ -0,0 +1,28 @@
+import phonenumbers as phn
+number = phn.parse('+48692137420', None)
+print(number)
+print("WDP jest najlepsze!!!")
+#--------------------------------------------------------------
+var_1 = 5
+var_2 = "Babcia"
+print(var_1)
+print(var_2)
+#--------------------------------------------------------------
+LICZBA_PI = 3.14
+#--------------------------------------------------------------
+if False:
+    print(var_2)
+if True:
+    pass
+#--------------------------------------------------------------
+class MyClass:
+    def method1(self):
+        pass
+    def method2(self):
+        pass
+    pass
+
+
+class MyClass2:
+    pass
+#--------------------------------------------------------------
